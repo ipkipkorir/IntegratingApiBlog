8ae7204ec7726190dab934b940011a0e6a37b9c3
McBKorir <34438978+ipkipkorir@users.noreply.github.com>
2024-04-29 00:38:51 +0300

    Update views.py
    Removed original APIs
    Added a new API

--- django_project/views.py
+++ django_project/views.py
@@ -2,16 +2,8 @@ from django.shortcuts import render
 import requests
 
 def index(request):
-  r1 = requests.get('https://api.github.com/events')
+  r1 = requests.get('https://api.reliefweb.int/v1/reports')
   data = r1.json()
-  events = data[0]['repo']
-  
-  r2 = requests.get('https://www.boredapi.com/api/activity')
-  data = r2.json()
-  activity  = data['activity']
-
-  r3 = requests.get('https://dog.ceo/api/breeds/image/random')
-  res3 = r3.json()
-  dog = res3['message']
+  reports = data[0]['title']
   
   return render(request, 'templates/index.html', {'events': events, 'activity': activity, 'dog': dog})
